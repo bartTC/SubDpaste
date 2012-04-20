3ffd55e39d31d266f2697d577b9d1cb56830d75b
Martin Mahner <martin@mahner.org>
2012-04-20 11:34:20 +0200

    Initial commit.

---
+++ dpaste.py
@@ -0,0 +1,37 @@
+import sublime, sublime_plugin
+
+import urllib
+import urllib2
+import sys
+
+sep = """
+
+
+------ 8< ------ 8< ------ 8< ------ 8< ------ 8< ------
+
+
+"""
+
+def paste_code(content):
+    request = urllib2.Request(
+        'http://dpaste.de/api/',
+        urllib.urlencode([('content', content)]),
+    )
+    response = urllib2.urlopen(request)
+    return response.read()[1:-1]
+
+
+class DpasteCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        regions = [region for region in self.view.sel() if not region.empty()]
+        regions = len(regions) == 0 and [sublime.Region(0, self.view.size())] \
+                                     or regions
+        region_data = [self.view.substr(region) for region in regions]
+        content = sep.join(region_data)
+
+        paste_url = paste_code(content)
+
+        sublime.status_message('Pasted to %s and copied to your clipboard!' % paste_url)
+        sublime.set_clipboard(paste_url)
+
+        return None
