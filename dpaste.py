b27b799b1695ec0c94d2cfb0b19a9aef60691755
Martin Mahner <martin@mahner.org>
2014-12-18 14:48:57 +0100

    Let the filename define the syntax automatically

--- dpaste.py
+++ dpaste.py
@@ -11,8 +11,13 @@ SEP = """
 
 """
 
-def paste_code(content):
-    data = urllib.parse.urlencode({'content': content})
+def paste_code(content, filename):
+    data = urllib.parse.urlencode({
+        'content': content,
+        'filename': filename,
+        'lexer': '',
+        'format': 'default',
+    })
     data = data.encode('utf8')
     response = urllib.request.urlopen(DPASTE_API_URL, data)
     return response.read()[1:-1]
@@ -25,8 +30,9 @@ class DpasteCommand(sublime_plugin.TextCommand):
                                      or regions
         region_data = [self.view.substr(region) for region in regions]
         content = SEP.join(region_data)
+        filename = self.view.file_name()
 
-        paste_url = paste_code(content)
+        paste_url = paste_code(content, filename)
         paste_url = paste_url.decode('utf8')
 
         sublime.status_message('Pasted to %s and copied to your clipboard!' % paste_url)
